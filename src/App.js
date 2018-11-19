8e4bd2b0c4d8703ceb764e975de7bb738870afae
rohit keshav <rohittk239@gmail.com>
2018-11-19 11:02:59 -0500

    added react router with example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./home";
+import Stuff from "./stuff";
+import Contact from "./contact";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1> Picsy </h1>
-          <p>
-            Image sharing app, built on top of Electron and ReactJS
-          </p>
-        </header>
-      </div>
+      <HashRouter>
+        <div>
+            <h1> Picsy </h1>
+            <p>
+              Image sharing app, built on top of Electron and ReactJS
+            </p>
+
+            <ul className="header">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/stuff">Stuff</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <div className="content">
+              <Route exact path="/" component={Home} />
+              <Route path="/stuff" component={Stuff} />
+              <Route path="/contact" component={Contact} />
+            </div>
+        </div>
+      </HashRouter>
     );
   }
 }
