2af54b1953a04ca6ad63a1ecf44b2e62e81498d4
rohit keshav <rohittk239@gmail.com>
2018-12-14 00:28:15 -0500

    auth api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,11 @@
 const imageRoutes = require("./image");
 const userRoutes = require("./user");
+const authRoutes = require("./login");
 
 const constructorMethod = app => {
+    app.use("/auth", authRoutes);
     app.use("/api/image", imageRoutes);
     app.use("/api/user", userRoutes);
-
     app.use("*", (req, res) => {
         res.status(404).json({ error: "Not found" });
     });
